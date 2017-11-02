0f0be2475c127a116548e1a20352fffd95bb6a6e
Hannes Achleitner <hannes.software@gmx.at>
2017-11-02 08:34:32 +0100

    Variant Trust initial
    remove crashlog
    
    rework version check
    
    fix authorithies
    
    fix git repo url
    
    show commit url
    
    show commit url #2
    
    add prominent license file
    
    doku about motivation and show upstream in about
    
    README layout
    
    README layout #2

--- wallet/src/de/schildbach/wallet/ui/AlertDialogsFragment.java
+++ wallet/src/de/schildbach/wallet/ui/AlertDialogsFragment.java
@@ -17,26 +17,6 @@
 
 package de.schildbach.wallet.ui;
 
-import java.io.BufferedReader;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
-import java.util.Iterator;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.common.base.Splitter;
-import com.squareup.okhttp.Call;
-import com.squareup.okhttp.HttpUrl;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import de.schildbach.wallet.Constants;
-import de.schildbach.wallet.WalletApplication;
-import de.schildbach.wallet.util.CrashReporter;
-import de.schildbach.wallet.R;
-
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.Fragment;
@@ -55,6 +35,23 @@ import android.os.HandlerThread;
 import android.os.Process;
 import android.text.format.DateUtils;
 
+import com.google.common.base.Splitter;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.util.Iterator;
+
+import de.schildbach.wallet.BuildConfig;
+import de.schildbach.wallet.Constants;
+import de.schildbach.wallet.R;
+import de.schildbach.wallet.WalletApplication;
+
 /**
  * @author Andreas Schildbach
  */
@@ -69,15 +66,15 @@ public class AlertDialogsFragment extends Fragment {
         }
     }
 
-    private Activity activity;
+    private Activity          activity;
     private WalletApplication application;
-    private PackageManager packageManager;
+    private PackageManager    packageManager;
 
     private HttpUrl versionUrl;
 
     private final Handler handler = new Handler();
     private HandlerThread backgroundThread;
-    private Handler backgroundHandler;
+    private Handler       backgroundHandler;
 
     private static final Logger log = LoggerFactory.getLogger(AlertDialogsFragment.class);
 
@@ -100,12 +97,11 @@ public class AlertDialogsFragment extends Fragment {
 
         final PackageInfo packageInfo = application.packageInfo();
         final int versionNameSplit = packageInfo.versionName.indexOf('-');
-        final HttpUrl.Builder url = HttpUrl
-                .parse(Constants.VERSION_URL
-                        + (versionNameSplit >= 0 ? packageInfo.versionName.substring(versionNameSplit) : ""))
+        final HttpUrl.Builder url = HttpUrl.parse(Constants.VERSION_URL.toString())
                 .newBuilder();
-        url.addEncodedQueryParameter("package", packageInfo.packageName);
-        url.addQueryParameter("current", Integer.toString(packageInfo.versionCode));
+        // Just use last forked version
+        url.addEncodedQueryParameter("package", "de.schildbach.wallet");
+        url.addQueryParameter("current", Integer.toString(318));
         versionUrl = url.build();
     }
 
@@ -164,7 +160,7 @@ public class AlertDialogsFragment extends Fragment {
                         }
                     }
                 } catch (final Exception x) {
-                    handleException(x);
+                    android.widget.Toast.makeText(getActivity(), x.getMessage(), android.widget.Toast.LENGTH_LONG);
                 }
                 if (!abort)
                     handleCatchAll();
@@ -256,16 +252,6 @@ public class AlertDialogsFragment extends Fragment {
         return false;
     }
 
-    private void handleException(final Exception x) {
-        if (x instanceof UnknownHostException || x instanceof SocketException || x instanceof SocketTimeoutException) {
-            // swallow
-            log.debug("problem reading", x);
-        } else {
-            CrashReporter.saveBackgroundTrace(new RuntimeException(versionUrl.toString(), x),
-                    application.packageInfo());
-        }
-    }
-
     private Dialog createTimeskewAlertDialog(final long diffMinutes) {
         final Intent settingsIntent = new Intent(android.provider.Settings.ACTION_DATE_SETTINGS);
         final DialogBuilder dialog = DialogBuilder.warn(activity, R.string.wallet_timeskew_dialog_title);
@@ -290,7 +276,7 @@ public class AlertDialogsFragment extends Fragment {
     private Dialog createVersionAlertDialog() {
         final Intent marketIntent = new Intent(Intent.ACTION_VIEW,
                 Uri.parse(String.format(Constants.MARKET_APP_URL, application.packageInfo().packageName)));
-        final Intent binaryIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.BINARY_URL));
+        final Intent binaryIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(BuildConfig.GIT_TAG_URL));
 
         final DialogBuilder dialog = DialogBuilder.warn(activity, R.string.wallet_version_dialog_title);
         final StringBuilder message = new StringBuilder(getString(R.string.wallet_version_dialog_msg));
