c22be6841df34054977f3d9b7574a08d0168001b
Hannes Achleitner <hannes.software@gmx.at>
2017-12-14 07:37:12 +0100

    improve BlockChainService Android O

--- wallet/src/de/schildbach/wallet/service/WlanReceiver.java
+++ wallet/src/de/schildbach/wallet/service/WlanReceiver.java
@@ -1,11 +1,12 @@
 package de.schildbach.wallet.service;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
-import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
 
 import de.schildbach.wallet.Configuration;
 
@@ -15,21 +16,30 @@ public class WlanReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
         if (action.equals(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION)) {
+            final Configuration config = new Configuration(PreferenceManager.getDefaultSharedPreferences(context), context.getResources());
+            final Intent intentB = new Intent(context, BlockchainServiceImpl.class);
+
             if (intent.getBooleanExtra(WifiManager.EXTRA_SUPPLICANT_CONNECTED, false)) {
                 // enter Wifi
+                if (!isMyServiceRunning(context, BlockchainServiceImpl.class)) {
+                    ContextCompat.startForegroundService(context, intentB);
+                }
             } else {
                 // WLAN lost
-                final Configuration config = new Configuration(PreferenceManager.getDefaultSharedPreferences(context), context.getResources());
                 if (config.isPeerWifiOnly()) {
-                    final Intent intentB = new Intent(context, BlockchainServiceImpl.class);
                     context.stopService(intentB);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        context.startForegroundService(intentB);
-                    } else {
-                        context.startService(intentB);
-                    }
                 }
             }
         }
+    }
+
+    private boolean isMyServiceRunning(Context context, Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
